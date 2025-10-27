aa42bdf9556247a0c4a98a9f8acb2d01d4581e67
Pedro Barbosa <p.brbsa@gmail.com>
2025-10-27 01:09:45 -0300

    feat: add index file to export shared helper functions for SWR

---
+++ src/hooks/shared/helpers/index.ts
@@ -0,0 +1,4 @@
+export { applyOptimisticUpdate } from "./applyOptimisticUpdate";
+export { createMutationError } from "./createMutationError";
+export { executeMutation } from "./executeMutation";
+export { rollbackOptimisticUpdate } from "./rollbackOptimisticUpdate";
