69982cd7cb7965548d2706c678b5191b0188d292
Pedro Barbosa <p.brbsa@gmail.com>
2025-10-27 01:08:14 -0300

    feat: add index file to export utility functions for SWR cache management

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export * from "./extractSWRKey";
+export * from "./mutateByGroup";
+export * from "./mutateById";
+export * from "./resetCache";
+export * from "./swrGetCache";
+export * from "./swrMutate";
+export * from "./to";
